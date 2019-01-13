ccfa32046603e19390058c2d87e5558b91b66f3c
Rallkus <serhuegi@gmail.com>
2019-01-13 13:37:45 +0100

    Changed SENDGRID_API_KEY variable in case there are any problems

--- backend/conduit/settings.py
+++ backend/conduit/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from secrets import SENDGRID_API_KEY
+from secrets import SENDGRID__API_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -152,5 +152,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
 }
 EMAIL_BACKEND = 'sgbackend.SendGridBackend'
-SENDGRID_API_KEY = SENDGRID_API_KEY
+SENDGRID_API_KEY = SENDGRID__API_KEY
 
